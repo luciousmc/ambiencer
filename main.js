28ff3d6d1f2f73b283762bbb3cc3937768a08091
Marlon <luciousmc@icloud.com>
2019-11-03 20:41:46 -0800

    adds smooth scrolling for sections when clicking the links

--- main.js
+++ main.js
@@ -2,5 +2,14 @@ $(document).ready(startApp);
 let clock = new Clock();
 
 function startApp(){
+    $('a').on('click', (event)=> {
+        if (event.currentTarget.hash !== ''){
+            const hash = event.currentTarget.hash;
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - $('.header-container').height()
+            }, 600)
+        }
+    })
     clock.init();
 }
