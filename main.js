42b6caf84b9a0f2206f5776640a4992bea91695a
Marlon <luciousmc@icloud.com>
2019-11-03 22:34:59 -0800

    adds different header text depending on mood set

--- main.js
+++ main.js
@@ -11,7 +11,56 @@ function startApp(){
                 scrollTop: $(hash).offset().top - $('.header-container').height()
             }, 600)
         }
-    })
-    
+    })    
     clock.init();
+}
+function switchMood (mood){
+    generateHeaderText(mood);
+}
+function generateHeaderText(mood){
+    const musicHeader = $('#music h1');
+    const poemHeader = $('#poem h1');
+    const newsHeader = $('#news h1');
+    const gifHeader = $('#gif h1');
+
+    switch(mood){
+        case 'happy':
+            musicHeader.text('Lift your spirit with a song');
+            poemHeader.text('Think happy thoughts');
+            newsHeader.text('Get some heartwarming news');
+            gifHeader.text('Here\'s a GIF to put a smile on your face');
+            break;
+        case 'sad':
+            musicHeader.text('Let the floodgates open..');
+            poemHeader.text('Let\'s go deep into the burrows of your heart');
+            newsHeader.text('Some stories sure to tug the heartstrings');
+            gifHeader.text('And a lonely GIF');
+            break;
+        case 'chill':
+            musicHeader.text('Relax your mind with some smooth beats');
+            poemHeader.text('Grab a cup o\' joe and have a seat');
+            newsHeader.text('Some casual reading for you');
+            gifHeader.text('How about a GIF from us to you, friend. -_o');
+            break;
+        case 'hype':
+            musicHeader.text('Let\'s get the party started..');
+            poemHeader.text('Legit poems to check out. Cop it, yo');
+            newsHeader.text('What\'s poppin\' in the news');
+            gifHeader.text('');
+            break;
+        case 'romantic':
+            musicHeader.text('Set the mood right with a song');
+            poemHeader.text('My cherie, amour');
+            newsHeader.text('Love is in the air. Check out these stories');
+            gifHeader.text('');
+            break;
+        case 'motivated':
+            musicHeader.text('Get pumped up');
+            poemHeader.text('Find inspiration from your soul');
+            newsHeader.text('Some articles to get you motivated');
+            gifHeader.text('');
+            break;
+        default:
+            break;
+    }
 }
