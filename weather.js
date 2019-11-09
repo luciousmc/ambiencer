ae1cf0bf551c366bf0260e5fb607cea04232f74c
Marlon <luciousmc@icloud.com>
2019-11-08 20:31:21 -0800

    adds method to get weather data adds weather element to main page to display weather data

--- weather.js
+++ weather.js
@@ -1,17 +1,35 @@
 class Weather {
     constructor(){
-        
+        this.getCurPosSuccess = this.getCurPosSuccess.bind(this);
     }
     getPositionFromBrowser(){
         if (navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(this.getCoords);
+            navigator.geolocation.getCurrentPosition(this.getCurPosSuccess, this.getCurPosFail);
         }
         else {
             alert('Sorry, geolocation has been disabled in this browser');
         }
     }
-    getCoords(geolocationObj){
-        console.log('the geo object: ', geolocationObj);
+    getCurPosSuccess(geolocationObj){
+        const longitude = geolocationObj.coords.longitude;
+        const latitude = geolocationObj.coords.latitude;
+
+        this.getWeather(latitude, longitude);
+    }
+    getCurPosFail(geolocationObj){
+        console.log('something went wrong: ', geolocationObj);
+    }
+    getWeather(latitude, longitude){
+        console.log('im in get weather: ',  latitude, longitude);
+        const forecastURL = '';
+        const temp = '';
+        const city = '';
+        const state = '';
+
+        $.getJSON(`https://api.weather.gov/points/${latitude},${longitude}`, data=>{
+            forecastURL = data.forecast;
+            ({city, state} = data.properties.relativeLocation.properties);
+        })
     }
     init(){
         this.getPositionFromBrowser();
