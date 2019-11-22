b3cc96bfb820b744670a9ea97265dd6c1f763bf3
Marlon <luciousmc@icloud.com>
2019-11-22 11:09:58 -0800

    adds key for image API

--- images.js
+++ images.js
@@ -1,5 +1,38 @@
 class ImageApi {
-    constructor(){
-        
+    constructor(keys){
+        this.moodVariations = {
+            happy: ['happy', 'cheerful'],
+            sad: ['sad', 'depressing'],
+            romantic: ['love', 'romantic', 'sappy'],
+            hype: ['hyphy', 'hype', 'party'],
+            chill: ['lo-fi', 'relaxing'],
+            motivated: ['party', 'motivational']
+        }
+        this.apiKey = keys.images;
     }
+    getImages(mood){
+        const imageParams = {
+            query: this.makeQuery(mood),
+            client_id: this.apiKey,
+            orientation: 'landscape',
+        }
+        const ajaxOptions = {
+            url: 'https://api.unsplash.com',
+            method: 'GET',
+            dataType: 'JSON',
+            // headers: {
+            //     'Accept-Version': 'v1'
+            // },
+            data: imageParams,
+        }
+        $.ajax(ajaxOptions)
+            .done((data)=>{
+                console.log('the data returned is: ', data);
+            });
+    }
+    makeQuery(mood){
+        let output = this.moodVariations[mood][0];
+        return output;
+    }
+    render(){}
 }
