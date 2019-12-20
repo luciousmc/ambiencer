48bb6fa81a3a43197bbf8520cfc365bc5ce91bf9
Marlon <luciousmc@icloud.com>
2019-12-19 19:25:21 -0800

    adds mood Variations object to gif API constructor

--- gifs.js
+++ gifs.js
@@ -2,7 +2,14 @@ class GifAPI {
     constructor(keys){
         this.endpoint = 'api.giphy.com/v1/gifs/search'
         this.apiKey = keys.gifs;
-
+        this.moodVariations = {
+            happy: ['happy', 'cheerful', 'laugh', 'smile'],
+            sad: ['sad', 'gloomy', 'sorrow'],
+            romantic: ['love', 'roses', 'sappy', 'romantic'],
+            hype: ['exciting', 'hype', 'party', 'rave'],
+            chill: ['lo-fi', 'bonfire', 'fireplace', 'cozy'],
+            motivated: ['confident', 'motivational']
+        }
     }
     getGifs(mood){
         const ajaxOptions = {
