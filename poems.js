08993564fca7a983189ddf13b740612732fcc43e
Marlon <luciousmc@icloud.com>
2019-12-20 21:18:09 -0800

    creates poems.js and adds class 'poemsAPI'

---
+++ poems.js
@@ -0,0 +1,18 @@
+class PoemsAPI{
+    constructor(){
+        this.endpoint = null;
+        this.apiKey = null;
+        this.moodVariations = {
+            happy: ['happy', 'cheerful', 'laugh', 'smile'],
+            sad: ['sad', 'gloomy', 'sorrow'],
+            romantic: ['love', 'roses', 'sappy', 'romantic'],
+            hype: ['exciting', 'hype', 'party', 'rave'],
+            chill: ['lo-fi', 'bonfire', 'fireplace', 'cozy'],
+            motivated: ['confident', 'motivational']
+        }
+    }
+    getPoems(mood){
+        
+    }
+    render(){}
+}
