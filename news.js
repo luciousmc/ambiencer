9adc2b529c62485f905752f1f0467e3a3c358fc0
Marlon <luciousmc@icloud.com>
2020-01-20 11:59:39 -0800

    moves article back button to bottom of article spaces article back button and read article button to edges of container

--- news.js
+++ news.js
@@ -60,6 +60,9 @@ class NewsApi {
         let articleContent = $('<p>').addClass('article-content').text(article.content);
         articleContentContainer.append(articleContent);
         container.append(articleContentContainer);
+
+        let buttonsContainer = $('<div>').addClass('buttons-container');
+        container.append(buttonsContainer);
         
         let backButtonContainer = $('<div>').addClass('back-button-container');
         let backButton = $('<div>').addClass('article-back-button')
@@ -70,7 +73,7 @@ class NewsApi {
                                             })
                                         })
         backButtonContainer.append(backButton);
-        container.append(backButtonContainer);
+        buttonsContainer.append(backButtonContainer);
 
         // Button that takes user to full article when clicked
         let readArticleContainer = $('<div>').addClass('read-article-button-container');
@@ -80,7 +83,7 @@ class NewsApi {
                                             window.open(article.url, '_blank');                                            
                                         })
         readArticleContainer.append(readArticleButton);
-        container.append(readArticleContainer);
+        buttonsContainer.append(readArticleContainer);
 
 
         // Place the article container inside of section with a fade in effect
