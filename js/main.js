b0a8b14b93e03f132f5a42d84b61bab43508013a
Marlon Clay <luciousmc@icloud.com>
2020-03-07 21:54:02 -0800

    creates App class and moves appropriate functions and properties

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 $(document).ready(startApp);
-let clock = new Clock();
-let weather = new Weather();
-let music = new MusicPlayer(apiKeys);
-let images = new ImageApi(apiKeys);
-let news = new NewsApi(apiKeys);
-let gifs = new GifAPI(apiKeys);
-let poems = new PoemsAPI();
 
 function startApp(){
     // SMOOTH SCROLLING 
@@ -17,84 +10,9 @@ function startApp(){
                 scrollTop: $(hash).offset().top - $('.header-container').height()
             }, 600)
         }
-    })    
-    addEventListeners();
-    clock.init();
-    weather.init();
-}
-function addEventListeners(){
-    // mood buttons click handler
-    $('.mood-button-container').on('click', 'button', (event)=>{
-        const moodText = event.currentTarget.firstChild.textContent;
-        switchMood(moodText.toLowerCase());
-        $('#main-page').fadeOut(700);
-        $('#content-page').fadeIn(700);
-    })
-    // change mood click handler
-    $('.change-mood').on('click', (event)=>{
-        $('#main-page').fadeIn(700);
-        $('#content-page').fadeOut(700);
-        $('.video-container').empty();
-        $('#news-content-container').empty().show();
-        $('#news-article-container').remove();
-        $('#gif-content-container').empty();
-        $('#poem-content-container').empty();
-        $('.poem-title-container').empty();
-    })
-}
-function switchMood (mood){
-    $('.current-mood').html(`${mood.toUpperCase()}`);
-    generateHeaderText(mood);
-    music.getMusic(mood);
-    images.getImages(mood);
-    news.getNews(mood);
-    gifs.getGifs(mood);
-    poems.getPoems(mood);
-}
-function generateHeaderText(mood){
-    const musicHeader = $('#music h1');
-    const poemHeader = $('#poem h1');
-    const newsHeader = $('#news h1');
-    const gifHeader = $('#gif h1');
-
-    switch(mood){
-        case 'happy':
-            musicHeader.text('Lift your spirit with a song');
-            poemHeader.text('Think happy thoughts');
-            newsHeader.text('Get some heartwarming news');
-            gifHeader.text('Smile!');
-            break;
-        case 'sad':
-            musicHeader.text('Let the floodgates open..');
-            poemHeader.text('Deep into the burrows of your heart');
-            newsHeader.text('Stories sure to tug the heartstrings');
-            gifHeader.text('And a lonely GIF');
-            break;
-        case 'chill':
-            musicHeader.text('Relax your mind');
-            poemHeader.text('Grab a cup o\' joe');
-            newsHeader.text('Some casual reading for you');
-            gifHeader.text('A GIF from us to you, friend. -_o');
-            break;
-        case 'hype':
-            musicHeader.text('Let\'s get the party started..');
-            poemHeader.text('Legit poems to check out');
-            newsHeader.text('What\'s poppin\' in the news');
-            gifHeader.text('Yeet');
-            break;
-        case 'romantic':
-            musicHeader.text('Set the mood right with a song');
-            poemHeader.text('My cherie, amour');
-            newsHeader.text('Love is in the air');
-            gifHeader.text('And Somthin\' special');
-            break;
-        case 'motivated':
-            musicHeader.text('Get pumped up');
-            poemHeader.text('Find inspiration from your soul');
-            newsHeader.text('Articles to motivate');
-            gifHeader.text('Do work');
-            break;
-        default:
-            break;
-    }
+    });
+    
+    // Instantiate the App
+    const app = new App(apiKeys);
+    app.init();
 }
